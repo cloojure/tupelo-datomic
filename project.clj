017bdbb3457d2a976d5cbde054b984370e944f8a
Alan Thompson <cloojure@gmail.com>
2016-03-09 17:00:38 -0800

    misc

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/cloojure/tupelo-datomic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [ [tupelo                           "0.1.64"]
+  :dependencies [ [tupelo                           "0.1.66"]
                   [org.clojure/clojure              "1.7.0"]
                   [org.clojure/core.incubator       "0.1.3"]
                   [org.clojure/test.check           "0.5.9"]

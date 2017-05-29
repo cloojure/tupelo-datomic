93fd7e31b59cbe459b32491e35e39fb756665830
Alan Thompson <cloojure@gmail.com>
2017-05-28 23:18:16 -0700

    Upgraded -> tupelo 0.9.41

---
+++ test/tst/tupelo_datomic/_bootstrap.cljc
@@ -0,0 +1,22 @@
+;   Copyright (c) Alan Thompson. All rights reserved.
+;   The use and distribution terms for this software are covered by the Eclipse Public License 1.0
+;   (http://opensource.org/licenses/eclipse-1.0.php) which can be found in the file epl-v10.html at
+;   the root of this distribution.  By using this software in any fashion, you are agreeing to be
+;   bound by the terms of this license.  You must not remove this notice, or any other, from this
+;   software.
+(ns tst.tupelo-datomic._bootstrap
+  "This namespace is used to perform one-time tasks during testing, such as printing the
+  Clojure version."
+  (:use clojure.test tupelo.test )
+  (:require [schema.core :as s]
+            [tupelo.core :as t] ))
+(t/refer-tupelo)
+
+; Prismatic Schema type definitions
+; #todo add to Schema docs
+(s/set-fn-validation! true) ; enforce fn schemas
+
+(dotest
+  (t/print-versions)
+ ;(spyx (s/fn-validation?))
+)
--- test/tst/tupelo_datomic/functionality.clj
+++ test/tst/tupelo_datomic/functionality.clj
@@ -1,76 +1,75 @@
 (ns tst.tupelo-datomic.functionality
   (:use clojure.test)
   (:require [tupelo-datomic.core  :as td]
-            ;[tupelo.schema        :as ts]
-            ;[datomic.api          :as d]
-            ;[schema.core          :as s]
-            ;[tupelo.core :as t]
+            [tupelo.schema        :as ts]
+            [datomic.api          :as d]
+            [schema.core          :as s]
+            [tupelo.core :as t]
   ))
-;(t/refer-tupelo)
+(t/refer-tupelo)
 
+(def datomic-uri "datomic:mem://tst.bond")      ; the URI for our test db
+(def ^:dynamic *conn*)                  ; dynamic var to hold the db connection
 
-;   (def datomic-uri "datomic:mem://tst.bond")      ; the URI for our test db
-;   (def ^:dynamic *conn*)                  ; dynamic var to hold the db connection
+;---------------------------------------------------------------------------------------------------
+; clojure.test fixture: setup & teardown for each test
+(use-fixtures :each
+  (fn setup-execute-teardown            ; perform setup, execution, & teardown for each test
+    [tst-fn]
+; setup ----------------------------------------------------------
+    (d/create-database datomic-uri)             ; create the DB
+    (try
+      (binding [*conn* (d/connect datomic-uri) ]  ; create & save a connection to the db
+; execute --------------------------------------------------------
+        (tst-fn))
+; teardown -------------------------------------------------------
+      (finally
+        (d/delete-database datomic-uri)))))
 
-;   ;---------------------------------------------------------------------------------------------------
-;   ; clojure.test fixture: setup & teardown for each test
-;   (use-fixtures :each
-;     (fn setup-execute-teardown            ; perform setup, execution, & teardown for each test
-;       [tst-fn]
-;   ; setup ----------------------------------------------------------
-;       (d/create-database datomic-uri)             ; create the DB
-;       (try
-;         (binding [*conn* (d/connect datomic-uri) ]  ; create & save a connection to the db
-;   ; execute --------------------------------------------------------
-;           (tst-fn))
-;   ; teardown -------------------------------------------------------
-;         (finally
-;           (d/delete-database datomic-uri)))))
+;---------------------------------------------------------------------------------------------------
+; Convenience function to keep syntax a bit more concise
+(defn live-db [] (d/db *conn*))
 
-;   ;---------------------------------------------------------------------------------------------------
-;   ; Convenience function to keep syntax a bit more concise
-;   (defn live-db [] (d/db *conn*))
+; helper function
+(s/defn get-people :- ts/Set
+  "Returns a set of entity maps for all entities with the :person/name attribute"
+  [db-val :- s/Any]
+  (let [eids  (td/find-attr   :let    [$ db-val]
+                              :find   [?eid]  ; <- could also use Datomic Pull API
+                              :where  {:db/id ?eid :person/name _} ) ]
+    (set  (for [eid eids]
+            (td/entity-map db-val eid)))))
 
-;   ; helper function
-;   (s/defn get-people :- ts/Set
-;     "Returns a set of entity maps for all entities with the :person/name attribute"
-;     [db-val :- s/Any]
-;     (let [eids  (td/find-attr   :let    [$ db-val]
-;                                 :find   [?eid]  ; <- could also use Datomic Pull API
-;                                 :where  {:db/id ?eid :person/name _} ) ]
-;       (set  (for [eid eids]
-;               (td/entity-map db-val eid)))))
+;---------------------------------------------------------------------------------------------------
+; #todo
+; demo to show changing an attribute (string -> integer, cardinality/one -> many, deleting an attribute
+; example of retracting 1 item from a normal (cardinality/one) attribute & a cardinality/many attribute
 
-;   ;---------------------------------------------------------------------------------------------------
-;   ; #todo
-;   ; demo to show changing an attribute (string -> integer, cardinality/one -> many, deleting an attribute
-;   ; example of retracting 1 item from a normal (cardinality/one) attribute & a cardinality/many attribute
+(deftest t-james-bond
 
-;   (deftest t-james-bond
+  ; Attributes can be "namespaced" in a variety of ways
+  (td/transact *conn*
+    (td/new-attribute   :person/name :db.type/string)
+    (td/new-attribute   :person.name :db.type/string)
+    (td/new-attribute   :person-name :db.type/string))
 
-;     ; Attributes can be "namespaced" in a variety of ways
-;     (td/transact *conn*
-;       (td/new-attribute   :person/name :db.type/string)
-;       (td/new-attribute   :person.name :db.type/string)
-;       (td/new-attribute   :person-name :db.type/string))
+  ; Re-creating attr is OK
+  (td/transact *conn*
+    (td/new-attribute   :person/name :db.type/string))
 
-;     ; Re-creating attr is OK
-;     (td/transact *conn*
-;       (td/new-attribute   :person/name :db.type/string))
+  ; Cannot change attr type (e.g. string -> long)
+  (let [result    (td/transact *conn*
+                    (td/new-attribute   :person.name :db.type/long)) ]
+    ; (newline) (spyx (class result))
+    ; (newline) (spyx result)
+    (is (re-find #":db.error/invalid-install-attribute" (.toString result))))
 
-;     ; Cannot change attr type (e.g. string -> long)
-;     (let [result    (td/transact *conn*
-;                       (td/new-attribute   :person.name :db.type/long)) ]
-;       ; (newline) (spyx (class result))
-;       ; (newline) (spyx result)
-;       (is (re-find #":db.error/invalid-install-attribute" (.toString result))))
+  (td/transact *conn*
+    (td/new-entity { :person/name "James Bond" :person.name "Jimmy" :person-name "Jack" } ))
 
-;     (td/transact *conn*
-;       (td/new-entity { :person/name "James Bond" :person.name "Jimmy" :person-name "Jack" } ))
+  (newline)
+  (is (= (spyx (get-people (live-db)))
+          #{ { :person/name "James Bond" :person.name "Jimmy" :person-name "Jack" } } ))
 
-;     (newline)
-;     (is (= (spyx (get-people (live-db)))
-;             #{ { :person/name "James Bond" :person.name "Jimmy" :person-name "Jack" } } ))
-
-;   ; #todo verify that datomic/q returns TupleSets (i.e. no duplicate tuples in result)
-;   )
+; #todo verify that datomic/q returns TupleSets (i.e. no duplicate tuples in result)
+)

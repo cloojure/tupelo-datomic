95d0f044b5dad4a9cdc986cbd150eea2660fb4d0
Alan Thompson <cloojure@gmail.com>
2019-02-11 15:47:58 -0800

    updated dependency versions

--- project.clj
+++ project.clj
@@ -3,22 +3,16 @@
   :url "http://github.com/cloojure/tupelo-datomic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [;[com.datomic/datomic-pro "0.9.5359" :exclusions [joda-time]]
-                 ;[com.datomic/client-cloud "0.8.50"]
-                 ;[com.datomic/datomic-free "0.9.5661"]
-
-                 [cheshire "5.8.0"]
-                 [criterium "0.4.4"]
+  :dependencies [ [cheshire "5.8.1"]
                  [danlentz/clj-uuid "0.1.7"]
                  [enlive "1.1.6"]
-                 ;[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojure "1.10.0"]
                  [org.clojure/data.xml "0.2.0-alpha5"]
-                 [org.clojure/spec.alpha "0.1.143"]
+                 [org.clojure/spec.alpha "0.2.176"]
                  [org.clojure/test.check "0.9.0"]
                  ;[org.clojure/tools.logging "0.4.0"]
-                 [prismatic/schema "1.1.8"]
-                 [tupelo "0.9.74"]
+                 [prismatic/schema "1.1.10"]
+                 [tupelo "0.9.128"]
                  ]
   :resource-paths ["resources/"
                    "resources/datomic-free-0.9.5661-everything.jar"
